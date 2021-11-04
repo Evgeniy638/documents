bccb26543c05753b607b3bebbe9f93dfd184c0ab
Evgeniy <56478221+Evgeniy638@users.noreply.github.com>
2021-11-04 18:37:14 +0300

    add save file to directory

--- src/main/java/com/evgeniy638/documents/controllers/FileController.java
+++ src/main/java/com/evgeniy638/documents/controllers/FileController.java
@@ -1,5 +1,7 @@
 package com.evgeniy638.documents.controllers;
 
+import com.evgeniy638.documents.services.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +12,17 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller()
 @RequestMapping("/files")
 public class FileController {
+    private final FileService fileService;
+
+    @Autowired
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
     @PostMapping("/upload")
-    public String upload(@RequestParam("file") MultipartFile file, Model model) {
-        model.addAttribute("file", file);
-        return "index";
+    public String upload(@RequestParam("file") MultipartFile file) {
+        System.out.println(file.getOriginalFilename());
+        fileService.save(file);
+        return "redirect:/home";
     }
 }
---
+++ src/main/java/com/evgeniy638/documents/services/FileService.java
@@ -0,0 +1,38 @@
+package com.evgeniy638.documents.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class FileService {
+    @Value("${app.path.upload.file}")
+    private String uploadPath;
+
+    @PostConstruct
+    public void init() {
+        try {
+            Files.createDirectories(Paths.get(uploadPath));
+        } catch (IOException e) {
+            throw new RuntimeException("Could not create upload folder!");
+        }
+    }
+
+    public void save(MultipartFile file) {
+        try {
+            Path root = Paths.get(uploadPath);
+            if (!Files.exists(root)) {
+                init();
+            }
+            Files.copy(file.getInputStream(), root.resolve(file.getOriginalFilename()));
+        } catch (Exception e) {
+            throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
+        }
+    }
+}
